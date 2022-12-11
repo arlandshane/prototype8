3c595354b5bc4115617e50c8eb2aaba611a5dbdd
arlandshane <73439798+arlandshane@users.noreply.github.com>
2022-12-11 14:36:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$("nav").click(function (e) {
+    $(".brands").css("opacity", "1");
+    $("p").css("margin", "15% 0 0 0");
+    $("p").text("O");
+    $("p").css("font-family", "Montserrat, serif");
+    $("p").css("font-size", "3rem");
+    $("p").css("font-family", "Montserrat, serif");
+    $("p").css("font-size", "2.2rem");
+    $("p").text("X");
+    $("p").css("margin", "0");
+    $("p").css("position", "inherit");
+    $("p").css("transform", "translate3d(0.57rem, 0.65rem, 0)");
+    $(".tile>img").css("transition-duration", "700ms").css("opacity", "1");
+    $(".tile").css("background", "none");
+    $("body").css("margin", "1%")
+    $("body").css("background-image", "linear-gradient(to right, rgb(255, 192, 249), pink, rgb(255, 192, 249))");
+    $(".tile").css("transition-duration", "700ms");
+    $(".1").css("transform", "translate3d(24rem, 0, 0)");
+    $(".2").css("transform", "translate3d(-30%, -23.4rem, 0)");
+    $(".3").css("transform", "translate3d(25%, 18.4rem, 0)");
+    $(".4").css("transform", "translate3d(-15.7rem, -5rem, 0)");
+    $(".5").css("transform", "translate3d(-23.5rem, 0, 0)");
+    $(".6").css("transform", "translate3d(16rem, -5rem, 0)");
+    $(".7").css("transform", "translate3d(37%, -23.2rem, 0)");
+    $(".8").css("transform", "translate3d(-25%, 18.4rem, 0)");
+});
